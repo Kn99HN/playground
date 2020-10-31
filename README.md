281de4d2ab02faa7fd29b3c570e0c917c42d37ff
Khanh Nguyen <khanhnguyen99hn@gmail.com>
2020-10-31 12:51:41 -0400

    Add Facebook questions

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MFWOC
+
 "I regret to inform you that we've decided to move forward with other candidates."
+
+# Description
+Rejections suck but hey, another opportunity to learn.
---
+++ src/Facebook.java
@@ -0,0 +1,34 @@
+package src;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Facebook {
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3};
+        printSubsetSum(numbers);
+    }
+
+    public static void printSubsetSum(int[] numbers) {
+        backtrack(numbers, new ArrayList<Integer>(), 0);
+    }
+
+    /**
+     * backtrack and append in DFS approach
+     * @param numbers
+     * @param currentNumbers
+     * @param index
+     */
+    public static void backtrack(int[] numbers, List<Integer> currentNumbers, int index) {
+        if(index >= numbers.length) {
+            return;
+        } else {
+            for(int i = index; i < numbers.length; i++) {
+                currentNumbers.add(numbers[i]);
+                System.out.println(currentNumbers.toString());
+                backtrack(numbers, currentNumbers, i + 1);
+                currentNumbers.remove(currentNumbers.size() - 1);
+            }
+        }
+    }
+
+}
